e32857441c6ca0ef509e2d2a35a36770249a3f92
Tyler Ruff <hello@blazed.space>
2022-06-20 03:43:49 -0400

    BLZ
    Initial release.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ function App() {
   return (
     <div className="App">
       <article>
-        <p>
+        <h1>
           Welcome to React Fire!
+        </h1>
+        <p>
+          Boilerplate framework for React.
         </p>
+        <img src="https://blazed.sirv.com/logo/Wallpaper-Beaker.png" />
       </article>
     </div>
   );
